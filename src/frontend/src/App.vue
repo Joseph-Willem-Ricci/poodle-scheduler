6b9773bade8e98812f2e89a5a8b8eb6bba637c81
Joseph Willem Ricci <josephwillemricci@gmail.com>
2023-12-28 12:03:06 -0500

    display of multiple submitted schedules + edit

--- src/frontend/src/App.vue
+++ src/frontend/src/App.vue
@@ -1,8 +1,22 @@
 <template>
   <div id="app">
-        <div class='wrap'>
-          <Calendar v-model="selectedDates" :minDate="nextMonthFirstDay" :maxDate="nextMonthLastDay" selectionMode="multiple"  @show="resetSelectedDates" />
+    <div class='wrap'>
+      <Calendar v-model="selectedDates" :minDate="nextMonthFirstDay" :maxDate="nextMonthLastDay" selectionMode="multiple" inline/>
+      <form @submit.prevent="handleSubmit">
+        <input v-model="name" placeholder="Name" required />
+        <input v-model.number="quota" placeholder="Quota" required />
+        <button type="submit">Submit Schedule</button>
+      </form>
+      <button @click="submitAll">Submit All</button>
+      <div class="availabilities">
+        <h2>Submitted Schedules</h2>
+        <div v-for="(availability, index) in availabilities" :key="index">
+          <h3>{{ availability.name }}</h3>
+          <p>Quota: {{ availability.quota }}</p>
+          <p>Dates: {{ availability.dates.sort((a, b) => a - b).map(date => date.getDate()).join(', ') }}</p>          <button @click="editSchedule(index)">Edit</button>
         </div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -17,7 +31,11 @@ export default {
     return {
       selectedDates: [],
       nextMonthFirstDay: null,
-      nextMonthLastDay: null
+      nextMonthLastDay: null,
+      name: '',
+      quota: null,
+      availabilities: [],
+      editIndex: null
     };
   },
   created() {
@@ -25,16 +43,36 @@ export default {
     const nextMonth = today.getMonth() + 1;
     this.nextMonthFirstDay = new Date(today.getFullYear(), nextMonth, 1);
     this.nextMonthLastDay = new Date(today.getFullYear(), nextMonth + 1, 0);
-    this.selectedDates = [this.nextMonthFirstDay];
   },
   methods: {
-    getNextMonthFirstDay() {
-      const today = new Date();
-      const nextMonth = new Date(today.getFullYear(), today.getMonth() + 1, 1);
-      return nextMonth;
+    handleSubmit() {
+      const availability = {
+        name: this.name,
+        quota: this.quota,
+        dates: this.selectedDates.map(date => new Date(date))
+      };
+      if (this.editIndex !== null) {
+        this.availabilities[this.editIndex] = availability;
+        this.editIndex = null;
+      } else {
+        this.availabilities.push(availability);
+      }
+      this.resetForm();
     },
-    resetSelectedDates() {
+    resetForm() {
+      this.name = '';
+      this.quota = null;
       this.selectedDates = [];
+    },
+    editSchedule(index) {
+      const availability = this.availabilities[index];
+      this.name = availability.name;
+      this.quota = availability.quota;
+      this.selectedDates = availability.dates;
+      this.editIndex = index;
+    },
+    submitAll() {
+      console.log(this.availabilities)
     }
   }
 }
@@ -46,7 +84,7 @@ export default {
     max-width: 250px;
 }
 .p-calendar .p-highlight {
-  background-color: #ff0000; /* Change this to your desired color */
+  background-color: #ff0000;
   color: #000000;
 }
 </style>
