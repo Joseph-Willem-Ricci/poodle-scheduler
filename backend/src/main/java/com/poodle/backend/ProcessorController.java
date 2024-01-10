102d9dd00f0d748e2e02622e8f38f8a959a37533
Joseph Willem Ricci <josephwillemricci@gmail.com>
2024-01-10 14:38:45 -0500

    edited api endpoint to /process & added more cross origins

--- backend/src/main/java/com/poodle/backend/ProcessorController.java
+++ backend/src/main/java/com/poodle/backend/ProcessorController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.*;
 @RequestMapping("/api")
 public class ProcessorController {
 
-    @CrossOrigin(origins = "https://www.poodlescheduler.com")  // TODO: Update this to the frontend URL
+    @CrossOrigin(origins = {"https://www.poodlescheduler.com", "http://www.poodlescheduler.com", "https://poodlescheduler.com", "http://poodlescheduler.com"})  // TODO: Update this to the frontend URL
     @PostMapping("/process")
     public String processFrontEndInput(@RequestBody ScheduleRequest request) {
         Processor processor = new Processor(request.getEmployeeList(), request.getNumberOfDays());
