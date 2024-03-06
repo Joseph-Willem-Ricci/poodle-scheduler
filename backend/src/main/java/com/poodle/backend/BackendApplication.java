a8aa175e44274b0e0f4101c43d5d7c0c89b78eb6
Joseph Willem Ricci <josephwillemricci@gmail.com>
2024-03-06 14:46:28 -0500

    set up koyeb DB, user&pass in env, and spring DB & security

--- backend/src/main/java/com/poodle/backend/BackendApplication.java
+++ backend/src/main/java/com/poodle/backend/BackendApplication.java
@@ -2,11 +2,16 @@ package com.poodle.backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class BackendApplication {
 
 	public static void main(String[] args) {
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DB_USERNAME", dotenv.get("DB_USERNAME"));
+		System.setProperty("DB_PASSWORD", dotenv.get("DB_PASSWORD"));
+
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
